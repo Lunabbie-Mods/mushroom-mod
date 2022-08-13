ad92eb42c928fb7395aebff8a21b1dffe5710ed6
luna <lunagibson@duck.com>
2022-08-13 12:09:50 +0100

    Set up basic fabric project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "mushroom-mod"
+// Required for fabric
+pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        gradlePluginPortal()
+    }
+}
+include("fabric")
