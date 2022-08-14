80e355af60058c14aee08ca8970e1b69f9b03110
Abbie <abigail@ient.me>
2022-08-14 13:36:30 +0100

    try to make it a food

--- fabric/src/main/java/com/lunabbie/mushroommod/ModItems.java
+++ fabric/src/main/java/com/lunabbie/mushroommod/ModItems.java
@@ -1,12 +1,16 @@
 package com.lunabbie.mushroommod;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
-    public static final Item PAINSHROOM = new Item(new FabricItemSettings().group(MushroomMod.ITEM_GROUP));
+    private static final FoodComponent PAINSHROOM_FOODCOMPONENT = new FoodComponent.Builder().hunger(0).saturationModifier(0.0f).statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_DAMAGE, 1, 0), 1.0f).build();
+    public static final Item PAINSHROOM = new Item(new FabricItemSettings().group(MushroomMod.ITEM_GROUP).food(PAINSHROOM_FOODCOMPONENT));
 
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier(MushroomMod.MODID, "painshroom"), PAINSHROOM);
