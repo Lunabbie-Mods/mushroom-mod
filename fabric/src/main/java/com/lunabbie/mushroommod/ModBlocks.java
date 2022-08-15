e45fe04064eac556b10c9b159992cafcd0fef61c
luna <lunagibson@duck.com>
2022-08-15 18:25:14 +0100

    Made painshroom transparent

--- fabric/src/main/java/com/lunabbie/mushroommod/ModBlocks.java
+++ fabric/src/main/java/com/lunabbie/mushroommod/ModBlocks.java
@@ -1,6 +1,8 @@
 package com.lunabbie.mushroommod;
 
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.block.*;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
@@ -8,9 +10,12 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.world.BlockView;
 
 public class ModBlocks {
-    public static Block PAINSHROOM = new MushroomPlantBlock(AbstractBlock.Settings.of(Material.PLANT, MapColor.PINK).noCollision().breakInstantly().sounds(BlockSoundGroup.GRASS).postProcess((BlockState state, BlockView world, BlockPos pos) -> true), null);
+    public static Block PAINSHROOM = new MushroomPlantBlock(AbstractBlock.Settings.of(Material.PLANT, MapColor.PINK).nonOpaque().noCollision().breakInstantly().sounds(BlockSoundGroup.GRASS).postProcess((BlockState state, BlockView world, BlockPos pos) -> true), null);
 
     public static void register() {
         Registry.register(Registry.BLOCK, new Identifier(MushroomMod.MODID, "painshroom"), PAINSHROOM);
+    }
+    public static void registerRendering() {
+        BlockRenderLayerMap.INSTANCE.putBlock(PAINSHROOM, RenderLayer.getCutout());
     }
 }
