b0f33e2cbcb9179ad14d11cfd84fb2109f9f324f
luna <lunagibson@duck.com>
2022-08-17 12:03:44 +0100

    Fixed byproduct of smelting Painshrooms to be MushroomBrew, not Painshrooms

--- fabric/src/main/java/com/lunabbie/mushroommod/mixin/AbstractFurnaceBlockEntityMixin.java
+++ fabric/src/main/java/com/lunabbie/mushroommod/mixin/AbstractFurnaceBlockEntityMixin.java
@@ -1,5 +1,6 @@
 package com.lunabbie.mushroommod.mixin;
 
+import com.lunabbie.mushroommod.items.MushroomBrew;
 import com.lunabbie.mushroommod.items.Painshroom;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.item.ItemStack;
@@ -17,7 +18,7 @@ public class AbstractFurnaceBlockEntityMixin {
     @Inject(method = "craftRecipe", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;decrement(I)V", shift = At.Shift.BEFORE))
     private static void brewBrew(@Nullable Recipe<?> recipe, DefaultedList<ItemStack> slots, int count, CallbackInfoReturnable<Boolean> cir) {
         if (slots.get(0).isOf(Painshroom.INSTANCE) && !slots.get(1).isEmpty() && slots.get(1).isOf(Items.BUCKET)) {
-            slots.set(1, new ItemStack(Painshroom.INSTANCE));
+            slots.set(1, new ItemStack(MushroomBrew.INSTANCE));
         }
     }
 }
