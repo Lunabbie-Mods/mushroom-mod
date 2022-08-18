96b05bfa625c87cf21da29120088a7dc4a76a530
Abigail Ient <abigail@ient.me>
2022-08-18 17:05:13 +0100

    Make mooshroom hostile when clicked on with painshroom
    hopefully this works, i cant test it

--- fabric/src/main/java/com/lunabbie/mushroommod/mixin/MooshroomEntityMixin.java
+++ fabric/src/main/java/com/lunabbie/mushroommod/mixin/MooshroomEntityMixin.java
@@ -1,7 +1,13 @@
 package com.lunabbie.mushroommod.mixin;
 
 import com.lunabbie.mushroommod.ModItems;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.ActiveTargetGoal;
+import net.minecraft.entity.ai.goal.AttackGoal;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.CowEntity;
 import net.minecraft.entity.passive.MooshroomEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -28,7 +34,8 @@ public abstract class MooshroomEntityMixin extends CowEntity {
             if (!player.getAbilities().creativeMode) {
                 handStack.decrement(1);
             }
-            // TODO do something
+            this.goalSelector.add(8, new MeleeAttackGoal(this, 1.0f, false));
+            this.targetSelector.add(1, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
             cir.setReturnValue(ActionResult.success(this.world.isClient));
         }
     }
