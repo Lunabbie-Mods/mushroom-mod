a32e80b941aa1a753d98c120734171ed5251a5e3
Abbie <abigail@ient.me>
2022-12-08 13:24:05 +0000

    WIP initial porting steps
    need to work on item group

--- fabric/src/main/java/com/lunabbie/mushroommod/ModBlocks.java
+++ fabric/src/main/java/com/lunabbie/mushroommod/ModBlocks.java
@@ -4,14 +4,15 @@ import com.lunabbie.mushroommod.blocks.PainshroomBlock;
 import com.lunabbie.mushroommod.blocks.PottedPainshroomBlock;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 
 import static com.lunabbie.mushroommod.MushroomModKt.identifier;
 
 public class ModBlocks {
     public static void register() {
-        Registry.register(Registry.BLOCK, identifier("painshroom"), PainshroomBlock.INSTANCE);
-        Registry.register(Registry.BLOCK, identifier("potted_painshroom"), PottedPainshroomBlock.INSTANCE);
+        Registry.register(Registries.BLOCK, identifier("painshroom"), PainshroomBlock.INSTANCE);
+        Registry.register(Registries.BLOCK, identifier("potted_painshroom"), PottedPainshroomBlock.INSTANCE);
     }
 
     public static void registerRendering() {
--- fabric/src/main/java/com/lunabbie/mushroommod/items/groups/ModItemGroup.kt
+++ fabric/src/main/java/com/lunabbie/mushroommod/items/groups/ModItemGroup.kt
@@ -1,11 +1,12 @@
 package com.lunabbie.mushroommod.items.groups
 
 import com.lunabbie.mushroommod.identifier
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
-import net.minecraft.util.registry.Registry
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
 
 abstract class ModItemGroup(val group: ItemGroup) {
 
@@ -15,14 +16,14 @@ abstract class ModItemGroup(val group: ItemGroup) {
         }
     }
 
-    constructor(key: String, icon: () -> ItemStack) : this(FabricItemGroupBuilder.build(
-        identifier(key), icon))
+    constructor(key: String, icon: () -> ItemStack) : this(FabricItemGroup.builder(
+        identifier(key)).icon(icon).build())
 
     private val _items = mutableListOf<Item>()
     val items: List<Item> = _items
 
     infix fun <T: Item> T.registeredAs(id: String): T {
-        Registry.register(Registry.ITEM, identifier(id), this)
+        Registry.register(Registries.ITEM, identifier(id), this)
         _items.add(this)
         this@registeredAs.group = this@ModItemGroup.group
         return this
